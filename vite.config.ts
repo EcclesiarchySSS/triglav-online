905cffc6d2e38ff533d5e98e13f9590cd8ae6fa1
EcclesiarchySSS <118938715+EcclesiarchySSS@users.noreply.github.com>
2025-12-15 12:16:20 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Важно для корректной работы на GitHub Pages
+  build: {
+    outDir: 'dist',
+  }
 })
